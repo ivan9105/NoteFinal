5944d9268746ddce45916a89a7b65fad282d319d
Ivan Melnikov <ivan9105@bk.ru>
2015-10-31 10:09:28 +0400

    add pacelable interface to model

--- app/src/main/java/note/com/notefinal/entity/Note.java
+++ app/src/main/java/note/com/notefinal/entity/Note.java
@@ -1,12 +1,15 @@
 package note.com.notefinal.entity;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 import java.util.UUID;
 
 /**
  * Created by Иван on 26.10.2015.
  */
-public class Note {
+public class Note implements Parcelable {
     private UUID id;
     private String title;
     private String description;
@@ -14,6 +17,20 @@ public class Note {
     private Tag tag;
 
     public Note() {
+    }
+
+    public Note(Parcel source) {
+        String[] data = new String[5];
+        source.readStringArray(data);
+
+        this.tag = new Tag();
+        tag.setId(UUID.fromString(data[3]));
+        tag.setName(data[4]);
+
+        this.id = UUID.fromString(data[0]);
+        this.title = data[1];
+        this.description = data[2];
+        this.date = new Date(source.readLong());
     }
 
     public UUID getId() {
@@ -55,4 +72,33 @@ public class Note {
     public void setTag(Tag tag) {
         this.tag = tag;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeStringArray(new String[]{
+                String.valueOf(id),
+                title,
+                description,
+                String.valueOf(tag.getId()),
+                tag.getName()
+        });
+        dest.writeLong(date.getTime());
+    }
+
+    public static final Parcelable.Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel source) {
+            return new Note(source);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[0];
+        }
+    };
 }
